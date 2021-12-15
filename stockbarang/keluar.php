540c5347d901ccfd7de5650e1d69fc19908c7c9d
putrijamalau23@gmail.com <putrijamalau23@gmail.com>
2021-12-15 12:30:33 +0700

    index

--- stockbarang/keluar.php
+++ stockbarang/keluar.php
@@ -37,7 +37,7 @@ if(empty($_SESSION['username'])){
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                        <a class="dropdown-item" href="../login/login.php">Logout</a>
+                        <a class="dropdown-item" href="../index.php">Logout</a>
                     </div>
                 </li>
             </ul>

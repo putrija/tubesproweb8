f1d6478940579d3779f56928bc7f9876e9f14942
ikramlubis <89634498+ikramlubis@users.noreply.github.com>
2021-12-23 21:41:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="shortcut icon" href="images/favicon (1).ico">
 
-    <title>EightO</title>
+    <title>EightO Homepage</title>
 </head>
 <body>
     <section>

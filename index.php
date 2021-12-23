c45cfa082b23a22f8e6a43750a63e537736ffdb4
ikramlubis4 <ikramlubis.mhd4@gmail.com>
2021-12-23 21:40:10 +0700

    ubah title /ikram

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="shortcut icon" href="images/favicon (1).ico">
 
-    <title>EightO Homepage</title>
+    <title>EightO</title>
 </head>
 <body>
     <section>

1d98fe2801a974c4887653df44a1bc5347da5fe7
putrijamalau23@gmail.com <putrijamalau23@gmail.com>
2021-12-29 13:21:34 +0700

    hapus modal

--- stockbarang/index.php
+++ stockbarang/index.php
@@ -141,32 +141,3 @@ if($_SESSION['level'] == 'user'){
     </div>
 </div>
 <?php include "include/footer.php"; ?>
-
-<!-- The Modal -->
-<div class="modal fade" id="myModal">
-    <div class="modal-dialog">
-    <div class="modal-content">
-    
-    <!-- Modal Header -->
-    <div class="modal-header">
-    <h4 class="modal-title">Tambah Barang</h4>
-    <button type="button" class="close" data-dismiss="modal">&times;</button>
-    </div>
-    
-    <!-- Modal body -->
-    <form method="POST">
-    <div class="modal-body">
-        <input type="text" name="namabarang" placeholder="Nama Barang" class="form-control" required>
-        <br>
-        <input type="text" name="deskripsi" placeholder="Deskripsi Barang" class="form-control" required>
-        <br>
-        <input type="number" name="stock" placeholder="stock" class="form-control" required>
-        <br>
-        <button type="submit" class="btn btn-primary" name="tambahbarangbaru">Submit</button>
-    </div>
-    </form>
-        
-    </div>
-    </div>
-</div>
-

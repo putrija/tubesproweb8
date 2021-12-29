43de302c0cae17a83ada858125f70b2ee61c4b39
AgnesTS <agnestryanisim@gmail.com>
2021-12-29 13:47:26 +0700

    carousel

--- index.php
+++ index.php
@@ -30,6 +30,51 @@
       <div class="imgbox">
         <img src="images/apelss.png">
       </div>
+      <div class="container" style="background:transparent ; padding-bottom: 0px;">	
+				<div id="myCarousel" class="carousel slide" dataride="carousel">
+					<!-- Indicators -->
+					<ol class="carousel-indicators">
+						<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+						<li data-target="#myCarousel" data-slide-to="1"></li>
+						<li data-target="#myCarousel" data-slide-to="2"></li>
+					</ol>
+					<!-- deklarasi carousel -->
+					<div class="carousel-inner" role="listbox">
+						<div class="item active">
+							<img src="images/mac3.jpg" style="height:650px;    width: 100%;">
+							<div class="carousel-caption">
+								
+							</div>
+							
+						</div>
+						<div class="item">
+							<img src="images/macbook-pro-16.jpg" style="height:650px;    width: 100%;">
+							
+							
+						</div>
+						<div class="item">
+							<img src="images/iPhone12pro-13-scaled.jpg" style="height:650px;    width: 100%;">
+							
+						</div>
+					</div>
+					
+					<!-- Tanda Panah Previous dan Next -->
+					<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+					<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+					</a>
+					<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+					<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+					</a>
+				</div>
+		</div>
+	
+	<div class="jumbotron">
+		
+	</div>
+
+      
       <div class="textbox">
         <h2 class="selamat">SELAMAT DATANG DI EightO</h2>
         <p class="teks">EightO adalah website gudang barang elektronik bermerk Apple oleh Kelompok 8. </br>Silahkan masuk atau daftarkan akun Anda untuk mengakses stok barang.</p>
